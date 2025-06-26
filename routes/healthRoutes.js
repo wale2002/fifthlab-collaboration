e56aa9ef2da41ada9d50a6548b875734af166142
wale2002 <gemred2002@gmail.com>
2025-06-26 11:21:24 +0100

    update2

--- routes/healthRoutes.js
+++ routes/healthRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const router = express.Router();
 router.get("/", async (req, res) => {
-  res.status(200).json({ status: "success", message: "Server is up" });
+  res.status(200).json({ status: "successs", message: "Server is up" });
 });
 
 module.exports = router;
